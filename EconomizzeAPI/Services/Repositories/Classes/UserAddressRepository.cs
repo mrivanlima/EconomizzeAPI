2d7ee0016ca8ddae28f85efcced1761f9219df48
Ivan Lima <mrivanlima@gmail.com>
2024-08-21 09:11:15 -0400

    Eliminate warnings

--- EconomizzeAPI/Services/Repositories/Classes/UserAddressRepository.cs
+++ EconomizzeAPI/Services/Repositories/Classes/UserAddressRepository.cs
@@ -6,6 +6,7 @@ using EconomizzeAPI.Services.Repositories.Interfaces;
 using Npgsql;
 using System.Data;
 
+
 namespace EconomizzeAPI.Services.Repositories.Classes
 {
     public class UserAddressRepository : IUserAddressRepository
@@ -33,21 +34,17 @@ namespace EconomizzeAPI.Services.Repositories.Classes
                 cmd.Parameters.AddWithValue("p_address_type_id", userAddress.AddressTypeId);
                 cmd.Parameters.AddWithValue("p_main_address", userAddress.MainAddress);
                 cmd.Parameters.AddWithValue("p_out_address_id", userAddress.AddressId).Direction = ParameterDirection.Output;
-
-
                 cmd.Parameters.AddWithValue("p_created_by", userAddress.CreatedBy);
                 cmd.Parameters.AddWithValue("p_modified_by", userAddress.ModifiedBy);
                 cmd.Parameters.AddWithValue("p_error", Error.HasError).Direction = ParameterDirection.InputOutput;
                 await _connection.OpenAsync();
-
                 cmd.ExecuteNonQuery();
 
-                Error.HasError = (bool)cmd.Parameters["p_error"].Value;
+                Error.HasError = (bool)(cmd.Parameters["p_error"].Value ?? false);
                 if (!Error.HasError)
                 {
-                    userAddress.AddressId = (int)cmd.Parameters["p_out_address_id"].Value;
+                    userAddress.AddressId = (int)(cmd.Parameters["p_out_address_id"].Value ?? -1);
                 }
-
             }
             catch (Exception ex)
             {
